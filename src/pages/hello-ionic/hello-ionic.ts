3230e3df181b04478e548dc1d22bda6152eb4295
thiagometal <thiagometal.fne@gmail.com>
2018-02-26 19:45:43 -0400

    cria metodo para chamar metodo get do provider de usuario

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { UserMetalProvider } from '../../providers/user-metal/user-metal';
 
 @Component({
   selector: 'page-hello-ionic',
-  templateUrl: 'hello-ionic.html'
+  templateUrl: 'hello-ionic.html',
+  providers: [UserMetalProvider]
 })
-export class HelloIonicPage {
-  constructor() {
 
+export class HelloIonicPage {
+  public usersMetal = [];
+
+  constructor(private userMetalService:UserMetalProvider) {
+    this.getUserMetal();
+  }
+
+  public getUserMetal(){
+    this.userMetalService.findAll().subscribe(response => this.usersMetal =  response);
   }
 }
