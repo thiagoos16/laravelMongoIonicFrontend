7661ecaff0cb9f56c3e0a38cab0a1b3edf11c996
thiagometal <thiagometal.fne@gmail.com>
2018-02-26 19:41:06 -0400

    adiciona provider para gerenciar utilizacao das rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { UserMetalProvider } from '../providers/user-metal/user-metal';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserMetalProvider,
+    UserMetalProvider
   ]
 })
 export class AppModule {}
---
+++ src/providers/user-metal/user-metal.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+/*
+  Generated class for the UserMetalProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class UserMetalProvider {
+
+  public urlUserMetal = "http://localhost:80000/user" 
+  
+  constructor(public http: HttpClient) {
+    console.log('Hello UserMetalProvider Provider');
+  }
+
+  public findAll():Observable<any> {
+    return this.http.get(this.urlUserMetal);
+  }
+
+}
